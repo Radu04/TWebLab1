76235bd026ef89f7fad0d0047e470a97e66281e3
Radu <emailul_tu@exemplu.com>
2025-03-07 16:06:16 +0200

    change project

--- js/2page.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.back-button').addEventListener('click', function(e) {
-    const confirmExit = confirm("Vrei să te întorci la pagina principală?");
-    if (!confirmExit) {
-        e.preventDefault(); 
-    }
-});
